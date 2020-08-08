299891ca4ed9258255210f80c164642f4d7ae9e4
GitHub <noreply@github.com>
2020-08-08 10:39:29 +0000

    Auto deploy from GitHub Actions build 58
    iBug: [062f371] Wiki: Fill index page

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -366,7 +366,7 @@ var store = [{
 "teaser": null
 },{
 "title": "中国科学技术大学 Linux 用户协会",
-"excerpt":"中国科学技术大学 Linux 用户协会（科大 LUG）是由在校的 GNU/Linux 爱好者发起并组成的全校性群众团体，是一个以推广自由软件文化、宣传自由软件价值为目的的协会组织。  了解更多  ","categories": [],
+"excerpt":"中国科学技术大学 Linux 用户协会（科大 LUG）是由在校的 GNU/Linux 爱好者发起并组成的全校性群众团体，是一个以推广自由软件文化、宣传自由软件价值为目的的协会组织。 了解更多 特色活动 LUG 每周小聚: 每周末由同学带来的技术分享演讲。 Linux Install Party: 推广和安装 Linux，宣传和鼓励开源和自由软件精神的活动。 Linux 101: 帮助新手学习 Linux 的系列活动。 自由软件日: 在国际自由软件基金会（SFI）的领导下，由自由/开源软件爱好者自发筹集的非盈利性活动。 信息安全大赛: 每年秋季学期举办的信息安全竞赛。 网络服务 开源软件镜像 LUG FTP(各种资源汇总) 网络启动服务 GitLab 代码托管 其他 请参阅 LUG 网络服务总则 联系我们 QQ 群: 适合于即时性较强的讨论，较正式的讨论还请使用邮件列表。具体群号请查看 qq.ustclug.org 的 TXT 记录。 邮件列表: ustc_lug (at) googlegroups.com 点击此处了解详情 IRC/Telegram...","categories": [],
 "tags": [],
 "url": "https://beta.ustclug.org//wiki/",
 "teaser": null
