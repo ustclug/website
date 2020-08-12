8fcf44b9582617782cae378a4b0520cf0b7ad3ef
iBug <git@ibugone.com>
2020-08-12 14:25:28 +0800

    This script should be working

--- _scripts/webhook.rb
+++ _scripts/webhook.rb
@@ -1,15 +1,14 @@
-
 require 'openssl'
 require 'net/http'
 require 'json'
 
 uri = URI("https://lug.ustc.edu.cn/_webhook/github/html")
 key = ENV['WEBHOOK_SECRET'] or exit
-payload = "sha1=#{JSON.dump({ :ref => "refs/heads/gh-pages" })}"
+payload = "#{JSON.dump({ :ref => "refs/heads/gh-pages" })}"
 
 req = Net::HTTP::Post.new uri
 req.body = payload
-req["X-Hub-Signature"] = OpenSSL::HMAC.hexdigest("SHA1", key, payload)
+req["X-Hub-Signature"] = "sha1=#{OpenSSL::HMAC.hexdigest "SHA1", key, payload}"
 req["Content-Type"] = "application/json"
 
 res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true, max_retries: 5) do |http|
