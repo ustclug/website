5f301f72e465227521d1a7906e90b9c29376a0c5
iBug <git@ibugone.com>
2020-08-12 14:21:32 +0800

    Wrong signature format

--- _scripts/webhook.rb
+++ _scripts/webhook.rb
@@ -5,7 +5,7 @@ require 'json'
 
 uri = URI("https://lug.ustc.edu.cn/_webhook/github/html")
 key = ENV['WEBHOOK_SECRET'] or exit
-payload = JSON.dump({ :ref => "refs/heads/gh-pages" })
+payload = "sha1=#{JSON.dump({ :ref => "refs/heads/gh-pages" })}"
 
 req = Net::HTTP::Post.new uri
 req.body = payload
