20f9a0a6dfd1f481973ea5f772bbd1d07c693403
GitHub <noreply@github.com>
2020-11-20 15:16:56 +0000

    Auto deploy from GitHub Actions build 180
    taoky: [fdf095dee] Add package.json

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1128,7 +1128,7 @@ var store = [{
 "teaser": null
 },{
 "title": "中国科学技术大学开源软件镜像",
-"excerpt":"mirrors.ustc.edu.cn 由中国科学技术大学、中国科学技术大学网络信息中心支持，USTC LUG 维护。 机器域名 IPv4/v6: mirrors.ustc.edu.cn （能解析出教育网/电信/移动/v6 地址） v4-only: ipv4.mirrors.ustc.edu.cn （能解析出教育网/电信/移动地址） v6-only: ipv6.mirrors.ustc.edu.cn 在有些地方 DNS 会解析出电信地址，但使用教育网地址访问更快，这时可以通过修改 hosts 指定强制使用教育网地址访问。 教育网 IP：202.38.95.110 电 信 IP：202.141.160.110 移 动 IP：202.141.176.110 IPv6：2001:da8:d800:95::110 支持的访问方式 http、https、ftp rsync - 部分 为什么？ 提供的服务 请访问 mirrors 主页以获取完整列表。 使用帮助 请使用开源镜像使用帮助列表，或点击主页源列表旁边的“Help”链接。 欢迎您协助我们更新使用帮助，请访问 LUG 的 GitHub 项目。我们对您的帮助表示感谢。 状态监控 在首页点击页面上方的同步状态链接，可以查看当前各镜像的同步状态，包括 成功与否/上游源/镜像体积等。 大家如果知道更好（更新、更快）的上游源，请联系我们。 如果同步出现故障，服务器的管理员会收到邮件通知，以尽快处理，所以大家看到状态...","categories": [],
+"excerpt":"mirrors.ustc.edu.cn 由中国科学技术大学、中国科学技术大学网络信息中心支持，USTC LUG 维护。 机器域名 IPv4/v6: mirrors.ustc.edu.cn（能解析出教育网/电信/移动/联通/v6 地址） v4-only: ipv4.mirrors.ustc.edu.cn（能解析出教育网/电信/移动/联通地址） v6-only: ipv6.mirrors.ustc.edu.cn 在有些地方 DNS 会解析出电信地址，但可能使用教育网地址访问更快，这时可以通过修改 hosts 指定强制使用教育网地址访问。 教育网 IP：202.38.95.110 (cernet.mirrors.ustc.edu.cn) 电信 IP：202.141.160.110 (chinanet.mirrors.ustc.edu.cn) 移动 IP：202.141.176.110 (cmcc.mirrors.ustc.edu.cn) 联通 IP：218.104.71.170 (unicom.mirrors.ustc.edu.cn) IPv6：2001:da8:d800:95::110 支持的访问方式 http、https rsync - 部分 为什么？ 提供的服务 请访问 mirrors 主页以获取完整列表。 使用帮助 请点击主页源列表旁边的“Help”链接，获取镜像使用帮助。 欢迎您协助我们更新使用帮助，请访问LUG 的 GitHub 项目 mirrorhelp。我们对您的帮助表示感谢。 状态监控 在首页点击页面上方的同步状态链接，可以查看当前各镜像的同步状态，包括 成功与否/上游源/镜像体积等。...","categories": [],
 "tags": [],
 "url": "https://lug.ustc.edu.cn//wiki/server/mirrors/",
 "teaser": null
