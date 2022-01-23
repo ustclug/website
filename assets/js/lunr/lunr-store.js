70004fde0b962f89a7676e1427f09d828caec3ab
GitHub <noreply@github.com>
2022-01-23 05:10:56 +0000

    Auto deploy from GitHub Actions build 395
    taoky: [1cbca8de8] Fix typo in NPM help

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1632,7 +1632,7 @@ var store = [{
 "teaser": null
 },{
 "title": "NPM 镜像使用帮助",
-"excerpt":"中科大镜像站的 npm 镜像是 https://registry.npmjs.org/ 的反代。   使用说明   编辑 ~/.npmrc ，添加   registry=https://npm.proxy.ustclug.org/   若不想将本源设置为默认源，只想使用本源安装某个软件包，可在安装包时采用以下用法：   npm --registry https://npm.proxy.ustclug.org/ install &lt;packagename&gt;   注意事项   不支持 publish，若出现错误，请将 ~/.npmrc 中的用户名密码部分注释掉，并删除缓存 ( rm -rf ~/.npm ) 重试。   相关链接   官方主页: https://www.npmjs.org/   ","categories": [],
+"excerpt":"中科大镜像站的 npm 镜像是 https://registry.npmjs.org/ 的反代。   使用说明   编辑 ~/.npmrc ，添加   registry=https://npmreg.proxy.ustclug.org/   若不想将本源设置为默认源，只想使用本源安装某个软件包，可在安装包时采用以下用法：   npm --registry https://npmreg.proxy.ustclug.org/ install &lt;packagename&gt;   注意事项   不支持 publish，若出现错误，请将 ~/.npmrc 中的用户名密码部分注释掉，并删除缓存 ( rm -rf ~/.npm ) 重试。   相关链接   官方主页: https://www.npmjs.org/   ","categories": [],
 "tags": [],
 "url": "/wiki/mirrors/help/npm/",
 "teaser": null
