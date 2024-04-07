9e54b30777fd90e2709390cbfff7340384d95fa6
GitHub <noreply@github.com>
2024-04-07 07:47:58 +0000

    Auto deploy from GitHub Actions build 585
    iBug: [98c9f52074] wiki: Update activity room number

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1380,7 +1380,7 @@ var store = [{
 "teaser": null
 },{
 "title": "中国科学技术大学 Linux 用户协会",
-"excerpt":"中国科学技术大学 Linux 用户协会（科大 LUG）是由在校的 GNU/Linux 爱好者发起并组成的全校性群众团体，是一个以推广自由软件文化、宣传自由软件价值为目的的协会组织。  了解更多   English Version   特色活动           LUG 每周小聚:       每周末由同学带来的技术分享演讲。            Linux Install Party:       推广和安装 Linux，宣传和鼓励开源和自由软件精神的活动。            Linux 101:       帮助新手学习 Linux 的系列活动。            自由软件日:       在国际自由软件基金会（SFI）的领导下，由自由/开源软件爱好者自发筹集的非盈利性活动。            信息安全大赛:       每年秋季学期举办的信息安全竞赛。       网络服务      开源软件镜像   LUG FTP(各种资源汇总)   网络启动服务   GitLab 代码托管   其他   请参阅 LUG 网络服务总则   联系我们      QQ 群: 适合于即时性较强的讨论，较正式的讨论还请使用邮件列表。具体群号请查看 qq.ustclug.org 的 TXT 记录。   邮件列表: ustc_lug (at) googlegroups.com 点击此处了解详情   IRC/Telegram 在线及时聊天，点击此处了解加入方式，平台互联由 TUNA 提供支持   瀚海星云 BBS: 瀚海星云 BBS Linux 版   官方邮箱:    活动室: 西区图书馆 206 室   更多联系方式请 戳这里   协会 LOGO      Designed by Shengyu Zhang on 2018-08-30   协会章程   LUG 的所有活动均依照协会章程开展。  ","categories": [],
+"excerpt":"中国科学技术大学 Linux 用户协会（科大 LUG）是由在校的 GNU/Linux 爱好者发起并组成的全校性群众团体，是一个以推广自由软件文化、宣传自由软件价值为目的的协会组织。  了解更多   English Version   特色活动           LUG 每周小聚:       每周末由同学带来的技术分享演讲。            Linux Install Party:       推广和安装 Linux，宣传和鼓励开源和自由软件精神的活动。            Linux 101:       帮助新手学习 Linux 的系列活动。            自由软件日:       在国际自由软件基金会（SFI）的领导下，由自由/开源软件爱好者自发筹集的非盈利性活动。            信息安全大赛:       每年秋季学期举办的信息安全竞赛。       网络服务      开源软件镜像   LUG FTP(各种资源汇总)   网络启动服务   GitLab 代码托管   其他   请参阅 LUG 网络服务总则   联系我们      QQ 群: 适合于即时性较强的讨论，较正式的讨论还请使用邮件列表。具体群号请查看 qq.ustclug.org 的 TXT 记录。   邮件列表: ustc_lug (at) googlegroups.com 点击此处了解详情   IRC/Telegram 在线及时聊天，点击此处了解加入方式，平台互联由 TUNA 提供支持   瀚海星云 BBS: 瀚海星云 BBS Linux 版   官方邮箱:    活动室: 西区图书馆 403 室   更多联系方式请 戳这里   协会 LOGO      Designed by Shengyu Zhang on 2018-08-30   协会章程   LUG 的所有活动均依照协会章程开展。  ","categories": [],
 "tags": [],
 "url": "/wiki/",
 "teaser": null
@@ -1500,7 +1500,7 @@ var store = [{
 "teaser": null
 },{
 "title": "联系我们",
-"excerpt":"官方邮件地址      您可以向我们直接发送电子邮件。   参与邮件列表讨论（推荐）   点击此处了解详情   Telegram 上的 LUG   加群链接见 jointele.ustclug.org 的 DNS TXT 记录   BBS 上的 LUG   前往瀚海星云 BBS Linux 版与我们讨论。   QQ 群上的 LUG   适合于即时性较强的讨论，较正式的讨论还请使用官方邮件列表。      QQ 群：群号请查看 qq.ustclug.org 的 DNS TXT 记录。校内学生请联系管理员了解详情。   IRC 上的 LUG      网络：OFTC   频道：#ustclug   面对面交流   USTC LUG 在校内的活动室：      西区：西区图书馆 206 室   东区：东区图书馆 605 室（已搬迁）   高新校区：高新图书馆 604B 研讨室   你也可以在 LUG 举办的各类活动中找到 LUG 的成员并当面进行交流。   QQ 公众号   QQ：2786229166      微信公众号   ID：ustclug  Name：中科大 LUG      社交网络上的 LUG   快来关注 SNS 上的 LUG 账户吧！LUG 活动通告，Linux 使用技巧、业界新闻应有尽有！    注意：以下账号目前缺乏维护。      Twitter: @USTCLUG   人人：USTCLUG   豆瓣：科大 LUG   更多信息，请参阅LUG 宣传账号一览。  ","categories": [],
+"excerpt":"官方邮件地址      您可以向我们直接发送电子邮件。   参与邮件列表讨论（推荐）   点击此处了解详情   Telegram 上的 LUG   加群链接见 jointele.ustclug.org 的 DNS TXT 记录   BBS 上的 LUG   前往瀚海星云 BBS Linux 版与我们讨论。   QQ 群上的 LUG   适合于即时性较强的讨论，较正式的讨论还请使用官方邮件列表。      QQ 群：群号请查看 qq.ustclug.org 的 DNS TXT 记录。校内学生请联系管理员了解详情。   IRC 上的 LUG      网络：OFTC   频道：#ustclug   面对面交流   USTC LUG 在校内的活动室：      西区：西区图书馆 403 室   西区：西区图书馆 206 室（已搬迁）   东区：东区图书馆 605 室（已搬迁）   高新校区：高新图书馆 604B 研讨室   你也可以在 LUG 举办的各类活动中找到 LUG 的成员并当面进行交流。   QQ 公众号   QQ：2786229166      微信公众号   ID：ustclug  Name：中科大 LUG      社交网络上的 LUG   快来关注 SNS 上的 LUG 账户吧！LUG 活动通告，Linux 使用技巧、业界新闻应有尽有！    注意：以下账号目前缺乏维护。      Twitter: @USTCLUG   人人：USTCLUG   豆瓣：科大 LUG   更多信息，请参阅LUG 宣传账号一览。  ","categories": [],
 "tags": [],
 "url": "/wiki/lug/contact/",
 "teaser": null
