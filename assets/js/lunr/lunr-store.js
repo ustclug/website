0b8480dbc4b93166b914c43ea0833049d29ababb
GitHub <noreply@github.com>
2024-05-29 06:30:05 +0000

    Auto deploy from GitHub Actions build 626
    iBug: [be8a37393f] _wiki/scripts: Replace table header

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1740,7 +1740,7 @@ var store = [{
 "teaser": null
 },{
 "title": "实用脚本集锦",
-"excerpt":"这里收集来自网络的各种优秀的脚本。                  USTC 网络通脚本       2016/09/13 19:12                               豆瓣电台       2010/11/25 19:41       Stephen Zhang                 You-Get       2013/04/29 16:04       Darcy Shen          ","categories": [],
+"excerpt":"这里收集来自网络的各种优秀的脚本。                  脚本       添加时间       作者                       USTC 网络通脚本       2016/09/13 19:12       LUG @ USTC                 豆瓣电台       2010/11/25 19:41       Stephen Zhang                 You-Get       2013/04/29 16:04       Darcy Shen          ","categories": [],
 "tags": [],
 "url": "/wiki/scripts/",
 "teaser": null
