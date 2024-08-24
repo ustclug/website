2d63e5e88982d863e2d7eb3b6a3c62f17fcaf39d
GitHub <noreply@github.com>
2024-08-24 19:19:25 +0000

    Auto deploy from GitHub Actions build 711
    iBug: [6dc99b77bb] lugftp: Change default link to HTTPS

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1692,7 +1692,7 @@ var store = [{
 "teaser": null
 },{
 "title": "LUG@USTC FTP 文件存档",
-"excerpt":"简介   LUG FTP 对应域名为 ftp.lug.ustc.edu.cn。   注意：直接在浏览器中输入这个域名可能会被识别为 FTP 协议。   这里存储社团历史活动资料的存档，以及一些与 Linux、开源软件、编程等计算机科学与技术相关的学习资料。   访问   使用 HTTP 协议   站点链接：https://ftp.lug.ustc.edu.cn   支持匿名只读访问。请注意部分目录无法使用 HTTP 访问。   使用 FTP 协议   站点链接：ftp://ftp.lug.ustc.edu.cn   不支持 FTP 匿名访问。用户须使用公开帐号登陆至服务器。   用户名为 ftp。请联系管理员以了解详细信息。   使用 SFTP 协议   用户名为 ftp。请联系管理员以了解详细信息。   上传   管理员用户   如需使用 FTP 协议对内容进行管理，请联系管理员以了解详细信息。     LUG@USTC  ","categories": [],
+"excerpt":"简介   LUG FTP 对应域名为 ftp.lug.ustc.edu.cn。   这里存储社团历史活动资料的存档，以及一些与 Linux、开源软件、编程等计算机科学与技术相关的学习资料。   访问   使用 HTTP 协议   站点链接：https://ftp.lug.ustc.edu.cn   支持匿名只读访问。请注意部分目录无法使用 HTTP 访问。   使用 FTP 协议   站点链接：ftp://ftp.lug.ustc.edu.cn   不支持 FTP 匿名访问。用户须使用公开帐号登陆至服务器。   用户名为 ftp。请联系管理员以了解详细信息。   使用 SFTP 协议   用户名为 ftp。请联系管理员以了解详细信息。   上传   如需使用 FTP 协议对内容进行管理，请联系管理员以了解详细信息。  ","categories": [],
 "tags": [],
 "url": "/wiki/lug/services/ftp/",
 "teaser": null
